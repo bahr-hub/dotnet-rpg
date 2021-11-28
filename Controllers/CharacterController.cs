bf3dafaaf935dfc83bbf9a506498a40a5e2f2896
bahr-hub <a.ah.ismail@gmail.com>
2021-11-28 16:44:27 +0200

    Put and Delet Character

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -39,5 +39,27 @@ namespace dotnet_rpg.Controllers
         {
             return Ok(await _characterService.AddCharacter(newCharacter));
         }
+
+        [HttpPut]
+        public async Task<ActionResult<ServiceResponse<GetCharacterDto>>> UpdateCharacter(UpdateCharacterDto updateCharacter)
+        {
+            var response = await _characterService.UpdateCharacter(updateCharacter);
+            if(response.Data == null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ServiceResponse<List<GetCharacterDto>>>> Delet(int id)
+        {
+            var response = await _characterService.DeleteCharacter(id);
+            if(response.Data == null)
+            {
+                return NotFound(response);
+            }
+            return Ok(response);
+        }
     }
 }
--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -10,5 +10,8 @@ namespace dotnet_rpg.Services.CharacterService
          Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharaters();
          Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
          Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter);
+         Task<ServiceResponse<GetCharacterDto>> UpdateCharacter(UpdateCharacterDto updatedCharacter);
+        Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter( int id);
+
     }
 }
